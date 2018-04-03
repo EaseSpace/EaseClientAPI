dc2b0f469157300eadb3b65cc61dda0ca98b0a81
titomheniart <heniart.thomas@gmail.com>
2018-04-03 13:59:27 +0000

    Generated code by SwaggerHub

---
+++ src/main/java/io/swagger/model/Customer.java
@@ -0,0 +1,120 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.CustomersAccounts;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Customer
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-04-03T13:59:24.359Z")
+
+public class Customer   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("accounts")
+  @Valid
+  private List<CustomersAccounts> accounts = null;
+
+  public Customer name(String name) {
+    this.name = name;
+    return this;
+  }
+
+   /**
+   * Get name
+   * @return name
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Customer accounts(List<CustomersAccounts> accounts) {
+    this.accounts = accounts;
+    return this;
+  }
+
+  public Customer addAccountsItem(CustomersAccounts accountsItem) {
+    if (this.accounts == null) {
+      this.accounts = new ArrayList<CustomersAccounts>();
+    }
+    this.accounts.add(accountsItem);
+    return this;
+  }
+
+   /**
+   * Get accounts
+   * @return accounts
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<CustomersAccounts> getAccounts() {
+    return accounts;
+  }
+
+  public void setAccounts(List<CustomersAccounts> accounts) {
+    this.accounts = accounts;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Customer customer = (Customer) o;
+    return Objects.equals(this.name, customer.name) &&
+        Objects.equals(this.accounts, customer.accounts);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, accounts);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Customer {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    accounts: ").append(toIndentedString(accounts)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ src/main/java/io/swagger/model/CustomersAccounts.java
@@ -0,0 +1,83 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.CustomersCredentials;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * CustomersAccounts
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-04-03T13:59:24.359Z")
+
+public class CustomersAccounts   {
+  @JsonProperty("credentials")
+  private CustomersCredentials credentials = null;
+
+  public CustomersAccounts credentials(CustomersCredentials credentials) {
+    this.credentials = credentials;
+    return this;
+  }
+
+   /**
+   * Get credentials
+   * @return credentials
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public CustomersCredentials getCredentials() {
+    return credentials;
+  }
+
+  public void setCredentials(CustomersCredentials credentials) {
+    this.credentials = credentials;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CustomersAccounts customersAccounts = (CustomersAccounts) o;
+    return Objects.equals(this.credentials, customersAccounts.credentials);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(credentials);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class CustomersAccounts {\n");
+    
+    sb.append("    credentials: ").append(toIndentedString(credentials)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
